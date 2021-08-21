767e05bbf3151a32e72ad96db591e2dec9b79b64
Yatharth <yatharth1997@gmail.com>
2021-08-21 10:39:37 +0530

    Added file that globally specifies pin assignments

---
+++ pins.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+const int TTL_C_Capture_Pin = 10;
+const int TTL_C_Output_Pin = 11;
+const int TTL_C_Output_Pin = 0;
+
+
+//Error Pins
+const int Error1_Pin;
+const int Error2_Pin;
+const int Error3_Pin;
+const int Error4_Pin;
+const int Error5_Pin;
