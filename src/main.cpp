b7702ae9c63b88b7028a1c933775a0a7987c7392
Yatharth <yatharth1997@gmail.com>
2021-09-17 18:46:56 +0530

    Restructured and cleaned - for final tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-
-// Blink.ino OR blink.ino
+// Main file
 
 #include<Arduino.h>
-//#include "./../code/software/multi_tau.hpp"
-//#include "./../code/software/Lin_ACorr_RT_Teensy.hpp"
+#include <imxrt.h>
+
+#include "./../code/software/multi_tau.hpp"
 #include "./../code/hardware/pit.hpp"
 #include "./../code/hardware/pins.hpp"
 #include "./../code/hardware/qtmr1.hpp"
@@ -11,71 +11,92 @@
 #include "./../code/hardware/ledpanel.hpp"
 
 
+// The template parameters are defined in the build system
+MultiTau_ACorr_RT_Teensy<LIN_CORRS, SERIES_SIZE, BIN_RATIO> multitau;
+
 //Hardware Resource
-PITController<2> PI_t; //! PI_t Resource
-TMR1Controller TTL_c; //! TTL_c Resource
-
-//Software Resource
-#define LIN_CORRS 9
-#define SERIES_SIZE 27
-#define BIN_RATIO 3
-
+PITController<PIT_CHANNEL_IN_USE> PI_t; //!< PI_t Resource
+TMR1Controller TTL_c; //!< TTL_c Resource
 
 //Global volatile variables
-volatile counter_t Counter_val = 0; //! Stores the value read by the counter
-volatile bool Update_flag = false; //! Indicates if a new value has arrived from the counting module
-volatile unsigned int Update_count = 0; //! Stores the number of updates made on the correlator channels 
-
+volatile counter_t Counter_val = 0; //!< Stores the value read from the counter
+volatile bool Update_flag = false; //!< Indicates if a new value has arrived from the counting module
+volatile unsigned int Update_count = 0; //!< Stores the number of updates made on the correlator channels since the last serialout
 
 //Time Values
-const unsigned int SerialOut_CountMax = 100000; //! Serial output is done after these many updates
-const double Gate_time_us = 0.5; //! The gate time of TTL_C in microseconds (us) 
-const double Allowed_period_error_us = 0.1; //! Gate time precision error due to finite precision of timers.
+uint32_t SerialOut_After = 100000; //! Serial output is done after these many updates
+const double Gate_time_us = 500; //! The gate time of TTL_C in microseconds (us) 
+const double Allowed_period_error_us = 1; //! Gate time precision error due to finite precision of timers.
 
-#define PIT_TEST_TOOGLE_PIN1 3
-#define PIT_TEST_TOOGLE_PIN2 7
+//const double Gate_time_us = GATE_TIME; //! The gate time of TTL_C in microseconds (us) 
+//const double Allowed_period_error_us = ALLOWED_GATE_TIME_ERROR; //! Gate time precision error due to finite precision of timers.
 
+// Resources for Testing (temporary)
+#define CHANNEL_SIZE 171
+int array[CHANNEL_SIZE];
 
+//======================================================
+
+//   /$$$$$$  /$$$$$$  /$$$$$$$ 
+//  |_  $$_/ /$$__  $$| $$__  $$
+//    | $$  | $$  \__/| $$  \ $$
+//    | $$  |  $$$$$$ | $$$$$$$/
+//    | $$   \____  $$| $$__  $$
+//    | $$   /$$  \ $$| $$  \ $$
+//   /$$$$$$|  $$$$$$/| $$  | $$
+//  |______/ \______/ |__/  |__/
+/** @brief ISR function used for processing counter values. */
 void isr_fn() //__attribute__((interrupt ("FIQ")))
 {
- //1. PIT Starts Counting
- IMXRT_PIT_CHANNELS[2].TFLG = 1;
- //PI_t.clear_interrupt_flag(); → scoping issues
+  //1. PIT Starts Counting
+  IMXRT_PIT_CHANNELS[PIT_CHANNEL_IN_USE].TFLG = 1;
+  
+  //2. Starts counting again after reset
+  IMXRT_TMR1.CH[0].SCTRL &= ~TMR_SCTRL_IEF; //Clear Edge Flag
 
- //2. Wait for Capture (Until IEF is *NOT* set.)
- //while((IMXRT_TMR1.CH[0].SCTRL & TMR_SCTRL_IEF) == 0)
-  // { } //Wait
+  //3. Read Counter Value
+  Counter_val = IMXRT_TMR1.CH[0].CNTR;
+  
+  //4. Reset Couter
+  IMXRT_TMR1.CH[0].CNTR = 0; //Reset Counter to 0.
 
- //3. Starts counting again after reset
- IMXRT_TMR1.CH[0].SCTRL &= ~TMR_SCTRL_IEF; //Clear Edge Flag
- Counter_val = IMXRT_TMR1.CH[0].CNTR;
- IMXRT_TMR1.CH[0].CNTR = 0; //Reset Counter to 0.
+  //5. Assert Update Flag
+  Update_flag = true;
 
- Update_flag = true;
-
-digitalToggleFast(PIT_TEST_TOOGLE_PIN1);
-digitalToggleFast(PIT_TEST_TOOGLE_PIN2);
-//LEDPanel.toggle_all();
-
- //4. Print Counter Value
- //Serial.println(Counter_val);
+  //-6. Toggle Pin for debugging
+  #if ISR_PIN_TOGGLE == 1
+    digitalToggleFast(PIT_TEST_TOOGLE_PIN1);
+  #endif
 }
 
 
+//    /$$$$$$              /$$                        
+//   /$$__  $$            | $$                        
+//  | $$  \__/  /$$$$$$  /$$$$$$   /$$   /$$  /$$$$$$ 
+//  |  $$$$$$  /$$__  $$|_  $$_/  | $$  | $$ /$$__  $$
+//   \____  $$| $$$$$$$$  | $$    | $$  | $$| $$  \ $$
+//   /$$  \ $$| $$_____/  | $$ /$$| $$  | $$| $$  | $$
+//  |  $$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$/| $$$$$$$/
+//   \______/  \_______/   \___/   \______/ | $$____/ 
+//                                          | $$      
+//                                          | $$      
+//                                          |__/
+/** @brief Setup function for Initalization and setup.*/
 void setup()
 {
-  //1. PinModes
+  //1. LEDPanel Indicator Setup
   LEDPanel.init(); //Set Pinmode for all LEDs.
-  digitalWrite(LED_BUILTIN, HIGH);
-  //pinMode(TTL_C_PULSE_INPUT_PIN, INPUT);
-  pinMode(PIT_TEST_TOOGLE_PIN1, OUTPUT);
-  pinMode(PIT_TEST_TOOGLE_PIN2, OUTPUT);
-  digitalWriteFast(PIT_TEST_TOOGLE_PIN1, HIGH);
-  digitalWriteFast(PIT_TEST_TOOGLE_PIN2, HIGH);
+  LEDPanel.set(SETUP_LED);
+  LEDPanel.toggle_all_routine(500);
+  LEDPanel.toggle_all_routine(500);
 
-
-
-  //2. Serial Setup
+  //2. Pin Modes
+  #if ISR_PIN_TOGGLE == 1
+    pinMode(PIT_TEST_TOOGLE_PIN, OUTPUT);
+  #endif
+  pinMode(TTL_C_PULSE_INPUT_PIN, INPUT);
+  
+  //3. Serial Setup
   Serial.begin(115200);
   while(!Serial){}
   Serial.println("Serial Connected!");
@@ -86,6 +107,7 @@ void setup()
   PI_t.sel_FBUS_clock(); //Select 150MHz clock
   PI_t.interrupt(); //Enable Interrupts
   PI_t.set_interrupt(isr_fn, 255); //Set interrupt
+  // Colllect Errors
   Error_t er_invalid_gt = PI_t.set_gate_time(Gate_time_us);
   Error_t er_period_precision = Errors::Precison_Threshold(PI_t.period_error_us(), Allowed_period_error_us);
 
@@ -93,31 +115,52 @@ void setup()
   TTL_c.init();
   TTL_c.init_pins();
 
-  //5. XBAR Connection
-  Serial.println(PI_t.get_xbar_in_pin());
-  Serial.println(TTL_c.get_xbar_out_pin());
-
-  xbar_connect(PI_t.get_xbar_in_pin(), TTL_c.get_xbar_out_pin());
-
-  delay(5000);
   //6. Setup is complete
-  asm volatile ("dsb");
-  digitalWrite(SETUP_LED, LOW);
+  LEDPanel.toggle(SETUP_LED, 500);
+  LEDPanel.toggle(SETUP_LED, 500);
 
-  //7. Validation
+  LEDPanel.unset(SETUP_LED);
+
+  
+
+  //7. Error Validation Validation
   Errors::Validate(er_invalid_gt);
   Errors::Validate(er_period_precision);
-  Errors::Validate(Errors::Auto_MultiTau_Input_Validator<LIN_CHANNELS, SERIES_SIZE, BIN_RATIO>());
+  Errors::Validate(Errors::Auto_MultiTau_Input_Validator<LIN_CORRS, SERIES_SIZE, BIN_RATIO>());
+  LEDPanel.assert_errors();
+  asm volatile ("dsb");
 
-  //7. Indicate End of Setup
-  LEDPanel.unset_all();
-
+  //7. Testing block
+  for (int i=0; i < CHANNEL_SIZE; i++) 
+  {
+    array[i] = i;
+  }
+  
 } // End of setup()
 
+
+
+//   /$$                                    
+//  | $$                                    
+//  | $$        /$$$$$$   /$$$$$$   /$$$$$$ 
+//  | $$       /$$__  $$ /$$__  $$ /$$__  $$
+//  | $$      | $$  \ $$| $$  \ $$| $$  \ $$
+//  | $$      | $$  | $$| $$  | $$| $$  | $$
+//  | $$$$$$$$|  $$$$$$/|  $$$$$$/| $$$$$$$/
+//  |________/ \______/  \______/ | $$____/ 
+//                                | $$      
+//                                | $$      
+//                                |__/ 
+/** @brief Loop function that processes the updates from the counters. */
 void loop()
 {
-  
-  LEDPanel.set(LOOP_LED);
+  // Assert Loop LED Indicator if the device is not in error state
+  if(!LEDPanel.Error_State)
+  {
+    LEDPanel.set(LOOP_LED);
+    LEDPanel.min_bright_all();
+  }
+
   TTL_c.start();
   PI_t.start();
   asm volatile ("dsb");
@@ -130,78 +173,39 @@ void loop()
       Update_flag = false;
       Update_count++;
 
-      if(Update_count >= SerialOut_CountMax)
+      if(Update_count >= SerialOut_After)
       {
-        //Multitau.ch_out();
-        Update_count = 0;
-        Serial.println(Counter_val);
+        //Testing → Serial Out Block
+        Serial.write((char*)&(array), sizeof(int)*CHANNEL_SIZE);
+        Serial.print('\n');
         Serial.flush();
-      }
-    }
-  }
+      
+      }//Serial Block
+    
+    }//Update Block
+  
+  }//While Loop
 
-  // <<while loop breaks>> ↓
-  digitalWrite(LED_BUILTIN, HIGH);
+  
+  // <<while loop breaks>> (rare case)↓
   LEDPanel.unset(LOOP_LED);
+  LEDPanel.set_all();
   abort();
 } //End of loop()
 
 
-/** @brief Main Function*/
+
+//   /$$      /$$           /$$          
+//  | $$$    /$$$          |__/          
+//  | $$$$  /$$$$  /$$$$$$  /$$ /$$$$$$$ 
+//  | $$ $$/$$ $$ |____  $$| $$| $$__  $$
+//  | $$  $$$| $$  /$$$$$$$| $$| $$  \ $$
+//  | $$\  $ | $$ /$$__  $$| $$| $$  | $$
+//  | $$ \/  | $$|  $$$$$$$| $$| $$  | $$
+//  |__/     |__/ \_______/|__/|__/  |__/
+/** @brief Main Function. */
 int main()
 {
-    Serial.begin(115200);
-    //while(!Serial)
-
-    //Serial.println("Serial!");
-    
-    LEDPanel.init();
-    //pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(7, OUTPUT);
-    digitalWriteFast(3, HIGH);
-    digitalWriteFast(7, HIGH);
-    asm volatile ("dsb");
-    int i = -1;
-    while(++i < 5)
-    {
-      //digitalWriteFast(LED_BUILTIN, HIGH);
-      LEDPanel.set(LED_BUILTIN);
-      LEDPanel.set(LED_YELLOW);
-
-      digitalToggleFast(3);
-      digitalToggleFast(7);
-      delay(50);
-      LEDPanel.unset(LED_BUILTIN);
-      LEDPanel.unset(LED_YELLOW);
-      delay(50);
-    }
-    //LEDPanel.set_all();
-    LEDPanel.set(LED_RED);
-    LEDPanel.set(LED_YELLOW);
-    
-    delay(2000);
-    LEDPanel.unset(LED_RED);
-    LEDPanel.unset(LED_YELLOW);
-
-
-
     setup();
-
-    i = -1;
-    while(++i < 5)
-    {
-      //digitalWriteFast(LED_BUILTIN, HIGH);
-      LEDPanel.set(LED_BUILTIN);
-      LEDPanel.set(LED_YELLOW);
-
-      digitalToggleFast(3);
-      digitalToggleFast(7);
-      delay(50);
-      LEDPanel.unset(LED_BUILTIN);
-      LEDPanel.unset(LED_YELLOW);
-      delay(50);
-    }
-
     loop();
 }
